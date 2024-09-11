bdd25123e220f74a2ee3c4800d75c67a90d06c2f
Елена Владимировна <50554917+Skylin44@users.noreply.github.com>
2024-09-11 09:45:00 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = [1,2,3 ,'Слово']
+print(immutable_var)
+immutable_var[2] = 'lemon'
+mutable_list = [3, 22, 'appel', 'lemon', 'tree']
+print(mutable_list)
+mutable_list[0] = 'pear'
+print(mutable_list)
