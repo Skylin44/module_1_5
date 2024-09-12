2f9b44bd7571e620a44cb1346a2c24d02224ef53
Елена Владимировна <50554917+Skylin44@users.noreply.github.com>
2024-09-12 10:53:34 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
-immutable_var = [1,2,3 ,'Слово']
+immutable_var = (1,2,3 ,'Слово')
 print(immutable_var)
-immutable_var[2] = 'lemon'
+#immutable_var[0] = 4 # данная переменная является неизменяемым элементом
 mutable_list = [3, 22, 'appel', 'lemon', 'tree']
 print(mutable_list)
 mutable_list[0] = 'pear'
